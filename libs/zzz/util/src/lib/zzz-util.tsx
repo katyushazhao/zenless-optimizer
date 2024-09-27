2b1920be953845c1d549069309438ea305c9e0c6
AriaZhao <50545494+katyushazhao@users.noreply.github.com>
2024-09-27 14:28:43 +1000

    init zzz-util

---
+++ libs/zzz/util/src/lib/zzz-util.tsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled'
+
+const StyledZzzUtil = styled.div`
+  color: pink;
+`
+
+export function ZzzUtil() {
+  return (
+    <StyledZzzUtil>
+      <h1>Welcome to ZzzUtil!</h1>
+    </StyledZzzUtil>
+  )
+}
+
+export default ZzzUtil
