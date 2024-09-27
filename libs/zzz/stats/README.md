00ef16f10de3e3dc89e8263d6f9af27508db45fc
AriaZhao <50545494+katyushazhao@users.noreply.github.com>
2024-09-27 21:52:29 +1000

    init zzz-stats

---
+++ libs/zzz/stats/README.md
@@ -0,0 +1,7 @@
+# zzz-stats
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test zzz-stats` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/zzz/stats/src/lib/zzz-stats.tsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled'
+
+const StyledZzzStats = styled.div`
+  color: pink;
+`
+
+export function ZzzStats() {
+  return (
+    <StyledZzzStats>
+      <h1>Welcome to ZzzStats!</h1>
+    </StyledZzzStats>
+  )
+}
+
+export default ZzzStats
