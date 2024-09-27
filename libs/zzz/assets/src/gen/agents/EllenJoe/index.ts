a5b6816396c839f4860c7d65f822689550f4a4f4
AriaZhao <50545494+katyushazhao@users.noreply.github.com>
2024-09-28 00:55:16 +1000

    create placeholders

---
+++ libs/zzz/assets/src/gen/agents/EllenJoe/index.ts
@@ -0,0 +1,30 @@
+import icon from './icon.png'
+import basic1 from './basic1.png'
+import dodge1 from './dodge1.png'
+import assist1 from './assist1.png'
+import special1 from './special1.png'
+import chain1 from './chain1.png'
+import core1 from './core1.png'
+import mindscape1 from './mindscape1.png'
+import mindscape2 from './mindscape2.png'
+import mindscape3 from './mindscape3.png'
+import mindscape4 from './mindscape4.png'
+import mindscape5 from './mindscape5.png'
+import mindscape6 from './mindscape6.png'
+
+const data = {
+  icon,
+  basic1,
+  dodge1,
+  assist1,
+  special1,
+  chain1,
+  core1,
+  mindscape1,
+  mindscape2,
+  mindscape3,
+  mindscape4,
+  mindscape5,
+  mindscape6
+} as const
+export default data
