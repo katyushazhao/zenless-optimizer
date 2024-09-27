1a72dfaeaf9613498dcacaa6c2aad3deebdfb37f
AriaZhao <50545494+katyushazhao@users.noreply.github.com>
2024-09-28 00:23:33 +1000

    create indexes

--- libs/zzz/assets/src/index.ts
+++ libs/zzz/assets/src/index.ts
@@ -1,1 +1,37 @@
-export * from './lib/zzz-assets'
+import type {
+  WEngineKey,
+  DriveDiscSeriesKey,
+  DriveDiscPartitionKey,
+  AgentKey,
+} from '@genshin-optimizer/zzz/consts'
+import agents from './gen/agents'
+import wEngines from './gen/wEngines'
+import drivediscs from './gen/drivediscs'
+type agentAssetKey =
+  | 'icon'
+  | 'basic'
+  | 'dodge'
+  | 'assist'
+  | 'special'
+  | 'chain'
+  | 'core'
+  | 'mindscape1'
+  | 'mindscape2'
+  | 'mindscape3'
+  | 'mindscape4'
+  | 'mindscape5'
+  | 'mindscape6'
+
+export function agentAsset(ck: AgentKey, asset: agentAssetKey) {
+  return agents[ck][asset]
+}
+type WEngineAssetKey = 'icon' | 'cover'
+export function wEngineAsset(lck: WEngineKey, asset: WEngineAssetKey) {
+  return wEngines[lck][asset]
+}
+
+export function drivediscAsset(rk: DriveDiscSeriesKey, partitionKey: DriveDiscPartitionKey) {
+  const drivedisc = drivediscs[rk]
+
+  return drivedisc[partitionKey as keyof typeof drivedisc]
+}
