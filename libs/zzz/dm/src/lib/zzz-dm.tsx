5c6ef33a73566b5e325bb691cf35a12029f10a92
AriaZhao <50545494+katyushazhao@users.noreply.github.com>
2024-09-27 14:54:38 +1000

    init zzz-dm

---
+++ libs/zzz/dm/src/lib/zzz-dm.tsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled'
+
+const StyledZzzDm = styled.div`
+  color: pink;
+`
+
+export function ZzzDm() {
+  return (
+    <StyledZzzDm>
+      <h1>Welcome to ZzzDm!</h1>
+    </StyledZzzDm>
+  )
+}
+
+export default ZzzDm
