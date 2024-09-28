76b15bc091181ff05eab14919e654e3d3889ef28
AriaZhao <50545494+katyushazhao@users.noreply.github.com>
2024-09-28 21:00:10 +1000

    update migrate.ts

--- libs/zzz/db/src/Database/migrate.ts
+++ libs/zzz/db/src/Database/migrate.ts
@@ -1,36 +1,36 @@
 // MIGRATION STEP
 // 0. DO NOT change old `migrateVersion` calls
-// 1. Add new `migrateVersion` call within `migrateSr` function
+// 1. Add new `migrateVersion` call within `migrateZZZ` function
 // 2. Add new `migrateVersion` call within `migrateStorage` function
 // 3. Update `currentDBVersion`
 // 4. Test on import, and also on version update
 
 import type { DBStorage } from '@genshin-optimizer/common/database'
-import type { ISrObjectDescription } from '@genshin-optimizer/sr/srod'
-import type { ISroDatabase } from '../Interfaces'
+import type { IZZZObjectDescription } from '@genshin-optimizer/zzz/zzzod'
+import type { IZZZoDatabase } from '../Interfaces'
 
 export const currentDBVersion = 1
 
-export function migrateSr(
-  sr: ISrObjectDescription & ISroDatabase
-): ISrObjectDescription & ISroDatabase {
-  const version = sr.dbVersion ?? 0
+export function migrateZZZ(
+  zzz: IZZZObjectDescription & IZZZoDatabase
+): IZZZObjectDescription & IZZZoDatabase {
+  const version = zzz.dbVersion ?? 0
   // function migrateVersion(version: number, cb: () => void) {
-  //   const dbver = sr.dbVersion ?? 0
+  //   const dbver = zzz.dbVersion ?? 0
   //   if (dbver < version) {
   //     cb()
   //     // Update version upon each successful migration, so we don't
   //     // need to migrate that part again if later parts fail.
-  //     sr.dbVersion = version
+  //     zzz.dbVersion = version
   //   }
   // }
 
   // migrateVersion(2, () => {})
 
-  sr.dbVersion = currentDBVersion
+  zzz.dbVersion = currentDBVersion
   if (version > currentDBVersion)
     throw new Error(`Database version ${version} is not supported`)
-  return sr
+  return zzz
 }
 
 /**
