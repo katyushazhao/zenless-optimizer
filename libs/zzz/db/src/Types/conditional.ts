e90cf075d69b1cffa8781acff9cab5d87a232f14
AriaZhao <50545494+katyushazhao@users.noreply.github.com>
2024-09-28 22:28:57 +1000

    update conditional.ts

--- libs/zzz/db/src/Types/conditional.ts
+++ libs/zzz/db/src/Types/conditional.ts
@@ -1,18 +1,18 @@
 import type {
-  CharacterKey,
-  LightConeKey,
-  RelicSetKey,
-} from '@genshin-optimizer/sr/consts'
+  AgentKey,
+  WEngineKey,
+  DriveDiscSeriesKey,
+} from '@genshin-optimizer/zzz/consts'
 
-type SheetKey = CharacterKey | RelicSetKey | LightConeKey
+type SheetKey = AgentKey | DriveDiscSeriesKey | WEngineKey
 // Stored per-char loadout, so the dst is assumed to be the owning char
-// CharKey|'all' is the srcKey
+// CharKey|'all' is the zzzcKey
 // SheetKey is the SheetKey
 // condkey is the condKey
 // value is the condValue
 export type ConditionalValues = Partial<
   Record<
-    CharacterKey | 'all',
+    AgentKey | 'all',
     Partial<Record<SheetKey, { [condkey: string]: number }>>
   >
 >
