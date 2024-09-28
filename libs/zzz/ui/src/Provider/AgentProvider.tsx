ecadff4628e4c6492799da39a8ed076a13af9c0f
AriaZhao <50545494+katyushazhao@users.noreply.github.com>
2024-09-29 00:17:03 +1000

    feat: create AgentProvider.tsx

---
+++ libs/zzz/ui/src/Provider/AgentProvider.tsx
@@ -0,0 +1,19 @@
+import type { AgentKey } from '@genshin-optimizer/zzz/consts'
+import type { ReactNode } from 'react'
+import { useMemo, useState } from 'react'
+import type { AgentContextObj } from '../Context/AgentContext'
+import { AgentContext } from '../Context/AgentContext'
+
+export function AgentProvider({ children }: { children: ReactNode }) {
+  const [agentKey, setAgentKey] = useState<AgentKey | ''>('')
+  const agentContextObj: AgentContextObj = useMemo(
+    () => ({ agentKey, setAgentKey }),
+    [agentKey]
+  )
+
+  return (
+    <AgentContext.Provider value={agentContextObj}>
+      {children}
+    </AgentContext.Provider>
+  )
+}
