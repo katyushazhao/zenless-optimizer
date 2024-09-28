180d963fb50fc26c1577454fdb38d63f42d5751f
AriaZhao <50545494+katyushazhao@users.noreply.github.com>
2024-09-28 20:46:15 +1000

    init zzz-db

---
+++ libs/zzz/db/src/lib/zzz-db.tsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled'
+
+const StyledZzzDb = styled.div`
+  color: pink;
+`
+
+export function ZzzDb() {
+  return (
+    <StyledZzzDb>
+      <h1>Welcome to ZzzDb!</h1>
+    </StyledZzzDb>
+  )
+}
+
+export default ZzzDb
